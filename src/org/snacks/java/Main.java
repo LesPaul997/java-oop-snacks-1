e63f4ab8bf7030cdef4e881f214a0ffb1ab81795
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-15 08:27:42 +0200

    Main and Class Student

---
+++ src/org/snacks/java/Main.java
@@ -0,0 +1,16 @@
+package org.snacks.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Istanza classe Studente
+		Studente studente = new Studente ("Pippo", "Alighieri", 52);
+		System.out.println(studente.getAllInfo() + "\n");
+		
+		//Istanza classe ContoBancario
+		
+
+	}
+
+}
--- src/org/snacks/java/Studente.java
+++ src/org/snacks/java/Studente.java
@@ -2,10 +2,26 @@ package org.snacks.java;
 
 class Studente {
 
-	private String firstName;
-	private String lastName;
-	private int age;
+		private String firstName;
+		private String lastName;
+		private int age;
 	
+		Studente(String firstName, String lastName, int age) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+		
+		}
+	
+		String getAllInfo() {
+			return this.firstName + " " + this.lastName + "," + this.age + "anni";
+		}
+	
+		public static void main(String[] args) {
+			Studente studente = new Studente ("Pippo", "Alighieri", 52);
+			System.out.println(studente.getAllInfo());
+		}
+		
 	}
 
 
